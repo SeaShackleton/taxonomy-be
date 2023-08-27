2c41edd5db71c5d50ad898c8d592610e4db14d94
Christian Shackleton <cas7374@gmail.com>
2023-08-27 13:42:14 -0500

    added data
    Added fake data to get the UI right

--- app/Http/Controllers/TaxonomyController.php
+++ app/Http/Controllers/TaxonomyController.php
@@ -17,26 +17,68 @@ class TaxonomyController extends Controller
 		return '{
 			"name": "Life",
 			"taxons": [
-			{
-			  "name": "Eucarya"
-			},
-			{
-			  "name": "Archaea"
-			},   
-			{
-			  "name": "Bacteria",
-			  "taxons": [
-				{ "name": "Accounts payable", "value": 53010 },
-				{ "name": "Accrued salaries", "value": 23554 },
-				{ "name": "Unearned revenue", "value": 253322 },
-				{ "name": "Commercial paper", "value": 326008 },
-				{ "name": "Current portion of long-term debt", "value": 112431 },
-				{ "name": "Funds held for others", "value": 2500 },
-				{ "name": "Other current liabilities", "value": 71036 }
-			  ]	  
-			}
+				{
+					 "name": "Eucarya",
+					 "taxons": [
+						{
+							"name": "Animalia", 
+							"value": 51532,
+							"taxons": [
+								{
+									"name": "Chordata",
+									"value": 42653,
+									"taxons": [
+										{
+											"name": "Mammalia",
+											"value": 32556,
+											"taxons": [
+												{
+													"name": "Carnivora",
+													"value": 12365,
+													"taxons": [
+														{
+															"name": "Canidae",
+															"value": 2236,
+															"taxons": [
+																{
+																	"name": "Canis",
+																	"value": 423,
+																	"taxons": [
+																		{
+																			"name": "Lupus",
+																			"value": 1
+																		}
+																	]
+																}
+															]
+														}
+													]
+												}
+											]
+										}
+									]
+								}
+							]
+						}
+					 ]
+				},
+				{
+				  "name": "Archaea"
+				},   
+				{
+				  "name": "Bacteria",
+				  "taxons": [
+					{ "name": "Accounts payable", "value": 53010 },
+					{ "name": "Accrued salaries", "value": 23554 },
+					{ "name": "Unearned revenue", "value": 253322 },
+					{ "name": "Commercial paper", "value": 326008 },
+					{ "name": "Current portion of long-term debt", "value": 112431 },
+					{ "name": "Funds held for others", "value": 2500 },
+					{ "name": "Other current liabilities", "value": 71036 }
+				  ]	  
+				}
 			]
-			}';
+		}';
 	}
 	public function addTaxonomy(Request $request){
 		//$request->

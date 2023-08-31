78fcb7fb52953a4094608bf75e36215d5440eb65
Christian Shackleton <cas7374@gmail.com>
2023-08-31 18:58:52 -0500

    data model testing
    data model testing

--- app/Http/Controllers/TaxonomyController.php
+++ app/Http/Controllers/TaxonomyController.php
@@ -16,36 +16,45 @@ class TaxonomyController extends Controller
 		
 		return '{
 			"name": "Life",
+			"id": 1,
 			"taxons": [
 				{
 					 "name": "Eucarya",
+					 "id": 2,
 					 "taxons": [
 						{
 							"name": "Animalia", 
+							"id": 3,
 							"value": 51532,
 							"taxons": [
 								{
 									"name": "Chordata",
+									"id": 4,
 									"value": 42653,
 									"taxons": [
 										{
 											"name": "Mammalia",
+											"id": 5,
 											"value": 32556,
 											"taxons": [
 												{
 													"name": "Carnivora",
+													"id": 6,
 													"value": 12365,
 													"taxons": [
 														{
 															"name": "Canidae",
+															"id": 7,
 															"value": 2236,
 															"taxons": [
 																{
 																	"name": "Canis",
+																	"id": 8,
 																	"value": 423,
 																	"taxons": [
 																		{
 																			"name": "Lupus",
+																			"id": 9,
 																			"value": 1
 																		}
 																	]
@@ -63,18 +72,20 @@ class TaxonomyController extends Controller
 					 ]
 				},
 				{
-				  "name": "Archaea"
+				  "name": "Archaea",
+				  "id": 10
 				},   
 				{
 				  "name": "Bacteria",
+				  "id": 11,
 				  "taxons": [
-					{ "name": "Accounts payable", "value": 53010 },
-					{ "name": "Accrued salaries", "value": 23554 },
-					{ "name": "Unearned revenue", "value": 253322 },
-					{ "name": "Commercial paper", "value": 326008 },
-					{ "name": "Current portion of long-term debt", "value": 112431 },
-					{ "name": "Funds held for others", "value": 2500 },
-					{ "name": "Other current liabilities", "value": 71036 }
+					{ "name": "Accounts payable", "id": 12, "value": 53010 },
+					{ "name": "Accrued salaries", "id": 13, "value": 23554 },
+					{ "name": "Unearned revenue", "id": 14, "value": 253322 },
+					{ "name": "Commercial paper", "id": 15, "value": 326008 },
+					{ "name": "Current portion of long-term debt", "id": 16, "value": 112431 },
+					{ "name": "Funds held for others", "id": 17, "value": 2500 },
+					{ "name": "Other current liabilities", "id": 18, "value": 71036 }
 				  ]	  
 				}
 			]
